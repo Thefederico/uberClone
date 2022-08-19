06605d9a99c6d68d8dda02c8df20f17d4a6d3474
Thefederico <federico_upegui.dev@outlook.com>
2022-08-18 20:42:33 -0500

    feat: added a complete map like uber

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,16 +1,99 @@
-import React from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, {useEffect, useRef} from 'react';
+import {Platform} from 'react-native';
 import tw from 'twrnc';
-import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import MapView, {Marker} from 'react-native-maps';
+import MapViewDirections from 'react-native-maps-directions';
+import {useDispatch, useSelector} from 'react-redux';
+import {GOOGLE_MAPS_API_KEY} from '@env';
+import {
+  selectDestination,
+  selectOrigin,
+  setTravelTimeInformation,
+} from '../features/navigation/navSlice';
 
-const Map = () => (
-  <MapView
-    style={tw`flex-1`}
-    initialRegion={{
-      latitude: 37.78825,
-      longitude: -122.4324,
-      latitudeDelta: 0.0922,
-      longitudeDelta: 0.0421,
-    }}
-  />
-);
+const Map = () => {
+  const dispatch = useDispatch();
+
+  const origin = useSelector(selectOrigin);
+  const destination = useSelector(selectDestination);
+  const mapRef = useRef(null);
+
+  useEffect(() => {
+    if (!origin || !destination) {
+      return;
+    } else {
+      setTimeout(() => {
+        mapRef.current.fitToSuppliedMarkers(
+          ['origin', 'destination'],
+          {
+            edgePadding: {top: 50, right: 50, bottom: 50, left: 50},
+          },
+          800,
+        );
+      });
+    }
+  }, [destination, origin]);
+
+  useEffect(() => {
+    if (!origin || !destination) {
+      return;
+    }
+    const getTravelTime = async () => {
+      fetch(
+        `https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=${origin.description}&destinations=${destination.description}&key=${GOOGLE_MAPS_API_KEY}`,
+      )
+        .then(res => res.json())
+        .then(data =>
+          dispatch(setTravelTimeInformation(data.rows[0].elements[0])),
+        );
+    };
+    getTravelTime();
+  }, [origin, destination, GOOGLE_MAPS_API_KEY]);
+
+  return (
+    <MapView
+      ref={mapRef}
+      style={tw`flex-1`}
+      mapType={Platform.OS === 'ios' ? 'mutedStandard' : 'standard'}
+      initialRegion={{
+        latitude: origin.location.lat,
+        longitude: origin.location.lng,
+        latitudeDelta: 0.005,
+        longitudeDelta: 0.005,
+      }}>
+      {origin && destination && (
+        <MapViewDirections
+          origin={origin.description}
+          destination={destination.description}
+          apikey={GOOGLE_MAPS_API_KEY}
+          strokeWidth={3}
+          strokeColor="black"
+        />
+      )}
+      {origin?.location && (
+        <Marker
+          coordinate={{
+            latitude: origin.location.lat,
+            longitude: origin.location.lng,
+          }}
+          title="Origin"
+          description={origin.description}
+          identifier="origin"
+        />
+      )}
+      {destination?.location && (
+        <Marker
+          coordinate={{
+            latitude: destination.location.lat,
+            longitude: destination.location.lng,
+          }}
+          title="destination"
+          description={destination.description}
+          identifier="destination"
+        />
+      )}
+    </MapView>
+  );
+};
 export {Map};
