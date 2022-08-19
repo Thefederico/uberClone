bd42e586cca0fe0d7543ff564ab162529e95d648
Thefederico <federico_upegui.dev@outlook.com>
2022-08-18 20:45:30 -0500

    chore: added navigation function for options

--- src/components/NavOptions.js
+++ src/components/NavOptions.js
@@ -3,6 +3,8 @@ import {View, Text, FlatList, TouchableOpacity, Image} from 'react-native';
 import {Icon} from '@rneui/base';
 import {useNavigation} from '@react-navigation/native';
 import tw from 'twrnc';
+import {useSelector} from 'react-redux';
+import {selectOrigin} from '../features/navigation/navSlice';
 
 const data = [
   {
@@ -21,6 +23,7 @@ const data = [
 
 function NavOptions() {
   const navigation = useNavigation();
+  const origin = useSelector(selectOrigin);
 
   return (
     <FlatList
@@ -30,13 +33,16 @@ function NavOptions() {
       renderItem={({item}) => (
         <TouchableOpacity
           style={tw`w-40 p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2`}
-          onPress={() => navigation.navigate(item.screen)}>
-          <View>
+          onPress={() => navigation.navigate(item.screen)}
+          disabled={!origin}>
+          <View style={tw`${!origin && 'opacity-20'}`}>
             <Image
               style={{width: 120, height: 120, resizeMode: 'contain'}}
               source={{uri: item.image}}
             />
-            <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
+            <Text style={tw`mt-2 text-lg font-semibold text-gray-600`}>
+              {item.title}
+            </Text>
             <Icon
               style={tw`w-10 mt-4 p-2 rounded-full bg-black`}
               name="arrowright"
